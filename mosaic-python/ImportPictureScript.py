baf3379a3942e15ac1f0cfcdd8b131c68b5f6e37
julien-monniot <n.ceska@gmail.com>
2015-10-11 20:15:22 +0200

    Refactoring

--- mosaic-python/ImportPictureScript.py
+++ mosaic-python/ImportPictureScript.py
@@ -1,13 +1,14 @@
-import DatabaseManager, logging
+import DatabaseManager
+import logging
 
 databaseName = "events.db"
 event1 = "Concert_Mika_2012"
 
 dbMan = DatabaseManager.SqliteManager(databaseName)
 
-dbMan.CreateEvent(event1)
+dbMan.create_event(event1)
 
-dbMan.InsertPicture(event1, ["80", "Mika1.jpg"])
+dbMan.insert_picture(event1, ["80", "Mika1.jpg"])
 
 
 
