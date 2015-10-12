75063d2a95cadae2bae32209314f8840cf66be12
Toumassa <artogu@live.fr>
2015-10-12 23:58:17 +0200

    FileDAO implemented, creation of configuration file

---
+++ insacloud/config/config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+config_FileDAO = {
+	'posterStorageLocation' : '/BACKEND/STATIC/poster/',
+	'imageStorageLocation' : '/BACKEND/STATIC/event/images/'
+	}
+
+
+config_ImportEventDataService = {
+	'locations':['Lyon', 'Villeurbanne', 'Oullins', 'Decines', 'Vaulx-en-Velin', 'Bron', 'Grenoble', 'Vienne']
+	}
