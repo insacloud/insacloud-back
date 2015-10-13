d237fc27b741a4973c3c7a5ce858202b75009702
julien-monniot <n.ceska@gmail.com>
2015-10-13 19:16:37 +0200

    Several methods 'declaration' in ImageFormatter. Some more methids added/completed in Mosaic manager and some doc written for those who'd like to continue on this

--- mosaic-python/ImageFormatter.py
+++ mosaic-python/ImageFormatter.py
@@ -1,3 +1,5 @@
+# add licence from base project ? or wait til we've modified everything :P ?
+
 from PIL import Image
 import logging
 
@@ -5,7 +7,7 @@ import logging
 class ImageFormatter:
 
     def __init__(self, image_path):
-        self.image_path = ''
+        self.image_path = image_path
         try:
             self.image = Image.open(image_path)
         except IOError:
@@ -13,32 +15,36 @@ class ImageFormatter:
             raise IOError
 
     def process_image(self):
-        picture = self.__crop_image(self.image)
+        self.__crop_image(self.image)
         color = self.__get_main_color(self.image)
 
-        return [picture, color]
+        return color
 
-    def __crop_image(self, image):
-        im_width, im_height = image.size
+    def save_image(self, image_path):
+        self.__resize_image() #fixed size for now maybe 5x5 px ?
+        pass
+
+    """
+        Crop input image so that you get a square image (not resized yet, only croped)
+    """
+    def __crop_image(self):
+        im_width, im_height = self.image.size
 
         if im_width > im_height:
-            image = image.crop((im_width/2-im_height/2), 0, (im_width/2+im_height/2), im_height)
+            self.image = self.image.crop((im_width/2-im_height/2), 0, (im_width/2+im_height/2), im_height)
         elif im_width < im_height:
-            image = image.crop((im_height/2-im_width/2), 0, (im_height/2+im_width/2), im_width)
+            self.image = self.image.crop((im_height/2-im_width/2), 0, (im_height/2+im_width/2), im_width)
         else:
             pass
 
-        return image
-
-    def __get_main_color(self, image):
+    def __get_main_color(self):
         # For now, it uses the function from base-project
-
         r = 0
         g = 0
         b = 0
         count = 0
-        pix = image.load()
-        im_max_x, im_max_y = image.size
+        pix = self.image.load()
+        im_max_x, im_max_y = self.image.size
 
         for x in range(0, im_max_x):
             for y in range(0, im_max_y):
@@ -49,3 +55,10 @@ class ImageFormatter:
                 count += 1
 
         return float(r) / float(count), float(g) / float(count), float(b) / float(count)
+
+    """
+        resize input image so that it can be saved and then directly used in a mosaic if needed.
+        Recommanded size 5x5px (not sure though)
+    """
+    def __resize_image(self):
+        pass
