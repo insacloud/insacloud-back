f953a94366dceec0552d7c4351850fbd820d004b
Toumassa <artogu@live.fr>
2015-10-21 15:12:31 +0200

    Reorganisation

--- insacloud/services/dao/FileDAO.py
+++ insacloud/services/dao/FileDAO.py
@@ -2,10 +2,12 @@
 import sys
 import urllib2
 import os
-sys.path.append("..")
-sys.path.append("config")
-from config import config_FileDAO
 
+sys.path.append("..")
+sys.path.append("../../services")
+sys.path.append("services/config")
+
+from import_config import config_FileDAO
 
 def StorePoster(idEvent, urlPoster):
 	# file to be written to
