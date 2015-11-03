243eaa81aab45e5ad667d0070868d5d4f5784d33
Mathieu Thoretton <mathieu@thoretton.com>
2015-11-03 11:34:03 +0100

    processing images on insert

--- insacloud/services/views.py
+++ insacloud/services/views.py
@@ -4,8 +4,8 @@ from rest_framework import viewsets
 from services.models import Event, Picture, Mosaic, Mosaic_cell
 from services.serializers import UserSerializer, GroupSerializer, EventSerializer, PictureSerializer, MosaicSerializer, Mosaic_cellSerializer
 from services.geolocalisation import get_bounding_box
+from services.mosaic.ImageFormatter import ImageFormatter
 from django.conf import settings
-
 
 class UserViewSet(viewsets.ModelViewSet):
   queryset = User.objects.all().order_by('-date_joined')
@@ -26,6 +26,10 @@ class EventViewSet(viewsets.ModelViewSet):
     fn, ext = os.path.splitext(event.poster.name)
     event.poster.name = settings.UPLOAD_PATH+"poster_{id}{ext}".format(id=event.id, ext=ext)
     os.rename(buff, event.poster.name)
+
+    imf = ImageFormatter(event.poster.name)
+    imf.save_image(event.poster.name)
+
     event.save()
 
   def get_queryset(self):
@@ -48,6 +52,12 @@ class PictureViewSet(viewsets.ModelViewSet):
     fn, ext = os.path.splitext(picture.image.name)
     picture.image.name = settings.UPLOAD_PATH+"{eventId}_{id}{ext}".format(id=picture.id, eventId=picture.event.id, ext=ext)
     os.rename(buff, picture.image.name)
+    resizeImage(picture.image.name)
+
+    imf = ImageFormatter(picture.image.name)
+    picture.hue = imf.process_image(settings.IMAGE_SIDE)
+    imf.save_image(picture.image.name)
+
     picture.save()
 
 class MosaicViewSet(viewsets.ModelViewSet):
