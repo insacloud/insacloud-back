ed87b0384313e45140a91540c20895477144ab81
mthoretton <mthoretton@gmail.com>
2015-11-08 23:28:19 +0100

    adding script to import pictures for event

---
+++ insacloud/services/mosaic/ImportPicturesForEvent.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import os
+from os import listdir
+from os.path import isfile, join
+import getpass
+import requests
+
+# Pictures have to be placed in folder named "pictures-to-import" in current path
+
+################################################################################
+####################################################################### Config #
+################################################################################
+
+urlInsaCloud = 'http://localhost/api/pictures/'
+apiInsacloud_user = input('Insacloud API user: ')
+apiInsacloud_pwd = getpass.getpass('Insacloud API password: ')
+eventId = input('Event id: ')
+picturesFolder = os.getcwd()+"/pictures-to-import/"
+
+print(picturesFolder)
+
+################################################################################
+####################################################################### Script #
+################################################################################
+
+files = [ f for f in listdir(picturesFolder) if isfile(join(picturesFolder,f)) ]
+
+data = {"event": eventId}
+for f in files:
+  print(f)
+  image = {'image': (f, open(picturesFolder+f, 'rb'), 'image/jpg', {'Expires': '0'})}
+  myResponse = requests.post(urlInsaCloud, data=data, files=image, auth=(apiInsacloud_user, apiInsacloud_pwd), verify=True)
+  print(myResponse.text)
