6ef4f4faecd556e90e8aa2e952ec1c26ac5512c5
MariaETEGAN <maria.etegan@insa-lyon.fr>
2015-11-11 13:55:22 +0100

    refactor generate mosaic

--- insacloud/services/mosaic/GenerateMosaic.py
+++ insacloud/services/mosaic/GenerateMosaic.py
@@ -15,7 +15,6 @@ class GenerateMosaic:
         self.zoom = 2
         # create mosaicDim*mosaicDim matrix
         mosaicMatrix = None
-        # TODO - Build this dictionnary
         self.hueMap = {}
         self.poster = Image.open(poster.path)
         self.pixel = self.poster.load()
@@ -84,63 +83,4 @@ class GenerateMosaic:
                 self.mosaicMatrix[x][y] = self.find_closest_available_hue(hue, self.hueMap)
 
         
-        self.generate_tile(event_id, imagesPath, 0,0,0)
-    
-
-    # generate tiles and mosaic
-    # might be functionalized (huge nb of vars+)
-    # for i in range(0, self.nbTilesPerDim, 1): 
-    #     for j in range(0, self.nbTilesPerDim, 1):          
-    #         tile = Image.new('L', (self.imageDim*self.tileDim,self.imageDim*self.tileDim)) 
-    #         tileMatrix = [[Image.new('L', (self.imageDim*self.nbTilesPerDim,self.imageDim*self.nbTilesPerDim)) for i in range(self.nbTilesPerDim)] for i in range(self.nbTilesPerDim)] 
-    #         for k in range(i*self.tileDim, (i+1)*self.tileDim, 1): 
-    #             for l in range(j*self.tileDim, (j+1)*self.tileDim, 1): 
-    #                 m = int((k%self.tileDim) / self.nbTilesPerDim) 
-    #                 n = int((l%self.tileDim) / self.nbTilesPerDim)  
-    #                 q = k % self.nbTilesPerDim  # 0..3
-    #                 r = l % self.nbTilesPerDim 
-    #                 tileMatrix[m][n].paste(self.mosaicMatrix[k][l],(q*self.imageDim,r*self.imageDim,(q+1)*self.imageDim,(r+1)*self.imageDim))
-    #                 # build tile with merging images
-    #                 tile.paste(self.mosaicMatrix[k][l], ((k%self.tileDim)*self.imageDim, (l%self.tileDim)*self.imageDim,((k%self.tileDim)+1)*self.imageDim, ((l%self.tileDim)+1)*self.imageDim))
-    #         # resize tile image
-    #         tile = tile.resize((self.imageDim,self.imageDim), Image.ANTIALIAS)
-    #         for x in range(0, self.nbTilesPerDim, 1):
-    #             for y in range(0, self.nbTilesPerDim, 1):
-    #                 tileMatrix[x][y] = tileMatrix[x][y].resize((self.imageDim,self.imageDim), Image.ANTIALIAS)
-    #                 name = "tile_"+ str(event_id)+"-"+str(i * self.nbTilesPerDim + x)+"-"+str(j * self.nbTilesPerDim + y)+".jpg"
-    #                 path = imagesPath + name
-    #                 tileMatrix[x][y].save(path, "JPEG")
-    #                 mosaic = Mosaic()
-    #                 mosaic.event = Event.objects.get(pk=event_id)
-    #                 mosaic.level = 2
-    #                 mosaic.row = i * self.nbTilesPerDim + x
-    #                 mosaic.column = j * self.nbTilesPerDim + y
-    #                 mosaic.image.name = name
-    #                 mosaic.save()
-
-    #         # build mosaic with merging tiles
-    #         self.mosaic.paste(tile, ((i%self.nbTilesPerDim)*self.imageDim, (j%self.nbTilesPerDim)*self.imageDim,((i%self.nbTilesPerDim)+1)*self.imageDim, ((j%self.nbTilesPerDim)+1)*self.imageDim))
-    #         # export mosaic image
-    #         name = "tile_"+str(event_id)+"-"+str(i)+"-"+str(j)+".jpg"
-    #         path = imagesPath+name
-    #         tile.save(path, "JPEG")
-
-    #         mosaic = Mosaic()
-    #         mosaic.event = Event.objects.get(pk=event_id)
-    #         mosaic.level = 1
-    #         mosaic.row = i
-    #         mosaic.column = j
-    #         mosaic.image.name = name
-    #         mosaic.save()
-
-    # # resize mosaic image
-    # self.mosaic = self.mosaic.resize((self.imageDim,self.imageDim), Image.ANTIALIAS)
-    # # export mosaic image
-    # name = "mosaic_"+str(event_id)+".jpg"
-    # path = imagesPath+name
-    # self.mosaic.save(path, "JPEG")
-    # mosaic = Mosaic()
-    # mosaic.level = 0
-    # mosaic.event = Event.objects.get(pk=event_id)
-    # mosaic.image.name = name
-    # mosaic.save()
+        self.generate_tile(event_id, imagesPath, 0,0,0)    
